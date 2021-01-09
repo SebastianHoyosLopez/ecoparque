cb272b0d0d80e93b265ce0188b9766e053c722f7
sebastianhoyoslopez <tatansebastan@gmail.com>
2021-01-09 13:15:23 -0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,25 @@ function App() {
   return (
     <Router>
       <NavbarHome />
-        <Switch>
-          <Route path="/" exact>
-            <Inicio />
-          </Route>
-          <Route path="/ecoparque" exact>
-            <Inicio />
-          </Route>
-          <Route path="/conocenos">
-            conocenos
-            <hr />
-            <div className="container-fluid bg-warning"></div>
-          </Route>
-          <Route path="/lugares">
-            lugares
-            <div className="container-fluid bg-danger"></div>
-          </Route>
-          <Route path="/contactanos">
-            Contáctanos
-          </Route>
-          <Route path="/reservas">
-            Reservas
-          </Route>
-        </Switch>
+      <Switch>
+        <Route path="/" exact>
+          <Inicio />
+        </Route>
+        <Route path="/ecoparque" exact>
+          <Inicio />
+        </Route>
+        <Route path="/conocenos">
+          conocenos
+          <hr />
+          <div className="container-fluid bg-warning"></div>
+        </Route>
+        <Route path="/lugares">
+          lugares
+          <div className="container-fluid bg-danger"></div>
+        </Route>
+        <Route path="/contactanos">Contáctanos</Route>
+        <Route path="/reservas">Reservas</Route>
+      </Switch>
     </Router>
   );
 }
